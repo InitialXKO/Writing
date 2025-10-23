70cc5ab79e532e655faf7853a6874b121e625077
InitialXKO <perfectko@msn.com>
2025-10-24 07:34:39 +0800

    恢复高级工具页面提示语为'高阶提示'，修改密码输入框标题为'不要恐慌'

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -46,7 +46,7 @@ export default function PasswordInput({ toolId, correctPassword, onUnlock }: Pas
         <div className="p-2 bg-morandi-purple-500/20 rounded-lg">
           <Lock className="w-5 h-5 text-morandi-purple-700" />
         </div>
-        超纲警告
+        不要恐慌
       </h2>
       <p className="text-morandi-purple-700 mb-4">
         此工具属于超纲内容，需要输入密码才能解锁
